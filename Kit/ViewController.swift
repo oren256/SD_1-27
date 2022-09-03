070c303c1ce7e889f366a5b5de120ff2cb0759f7
Ilya Yarichevskiy <akva3128@mail.ru>
2022-09-03 21:05:14 +0500

    Initial Commit

---
+++ Kit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kit
+//
+//  Created by  DNS on 03.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

d152e0510447eb415b2a512f8589394f47331409
Ilya Yarichevskiy <akva3128@mail.ru>
2022-09-09 13:33:03 +0500

    UISegmentControl

--- Kit/ViewController.swift
+++ Kit/ViewController.swift
@@ -8,10 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var segmentControll = UISegmentedControl()
+    var imageView = UIImageView()
+    
+    var menyArray = ["one", "two", "three"]
+    let imageArray = [UIImage(named: "boot1"),
+                      UIImage(named: "boot2"),
+                      UIImage(named: "boot3")]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //image
+        self.imageView.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        self.imageView.center = self.view.center
+        self.imageView.image = self.imageArray[2]
+        self.view.addSubview(self.imageView)
+        
+        //create segment
+        self.segmentControll = UISegmentedControl(items: self.menyArray)
+        self.segmentControll.frame = CGRect(x: 100, y: 700, width: 200, height: 50)
+        self.view.addSubview(self.segmentControll)
+        
     }
 
 

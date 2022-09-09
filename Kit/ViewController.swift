a04da93d98d3b7b265d1cd2abd2f0fdc5a42e4c4
Ilya Yarichevskiy <akva3128@mail.ru>
2022-09-09 19:23:33 +0500

    Task UIActivity

--- Kit/ViewController.swift
+++ Kit/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var buttonShare = UIButton()
+    var textField = UITextField()
+    var activityViewController: UIActivityViewController? = nil
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+    
+    //MARK: - Method
+    func createTextField() {
+        self.textField.frame = CGRect(x: 0, y: 0, width: 200, height: 30)
+        self.textField.center = self.view.center
+        self.textField.borderStyle = UITextField.BorderStyle.roundedRect
+        self.textField.placeholder = "Enter text to share"
+        self.view.addSubview(self.textField)
+    }
+    
+    func createButton() {
+        self.buttonShare = UIButton(type: .roundedRect)
+        self.buttonShare.frame = CGRect(x: 50, y: 350, width: 280, height: 44)
+        self.buttonShare.setTitle("Расшарить", for: .normal)
+        self.buttonShare.addTarget(self, action: #selector(handleShare), for: .touchUpInside)
+        self.view.addSubview(buttonShare)
+    }
+    
+    @objc func handleShare(paramSender: Any) {
+        let text = self.textField.text
+        
+        if text?.count == 0 {
+            let myMessage = "Сначало введите текст, потом нажмите кнопку"
+            let alertControler = UIAlertController(title: nil, message: myMessage, preferredStyle: .alert)
+            let action = UIAlertAction(title: "ok", style: .cancel)
+            alertControler.addAction(action)
+            self.present(alertControler, animated: true)
+        }
+        
+        self.activityViewController = UIActivityViewController(activityItems: [self.textField.text ?? "nil"], applicationActivities: nil)
+        self.present(self.activityViewController!, animated: true)
+    }
 
 }
 
